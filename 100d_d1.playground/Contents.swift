5c044a652c9842f264faa153297af03d082bc934
Piero Mamberti <piero.mamberti@gmail.com>
2020-05-16 16:18:28 +0100

    First push

---
+++ 100d_d1.playground/Contents.swift
@@ -0,0 +1,27 @@
+import UIKit
+
+var str = "Hello, playground"
+var age = 38
+var population = 8
+
+var str1 = """
+This goes \
+ober multiple
+lines
+"""
+
+var pi = 3.141
+var awesome = true
+
+var score = 85
+var str3 = "Your score was \(score)"
+
+let taylor = "swift"
+// taylor = "meh"
+
+let str4 = "Hello Playground!"
+let album: String = "Reputation"
+let year: Int = 1989
+let height: Double = 1.78
+let taylorRocks: Bool = true
+
